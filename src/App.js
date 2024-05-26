3af1d820431737dd669c6abbe9d2a27d601dd2e8
jennifer <jennifer.omena@outlook.com>
2024-05-26 01:01:42 -0300

    Criação da arquitetura inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <h1>Welcome to the Tech & Game Influencers Platform</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
