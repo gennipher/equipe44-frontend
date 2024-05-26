1a573b3f84905e89ad5bef030dcb4467f870caba
jennifer <jennifer.omena@outlook.com>
2024-05-26 14:31:04 -0300

    atualizando home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,10 @@ import Menu from '../../Components/Menu/Menu';
 import { Button } from 'react-bootstrap';
 import Imagem01 from "../../assets/image01.png";
 import Imagem02 from "../../assets/image02.png";
+import KabumLogo from "../../assets/kabumLogo.png";
+import DragonLogo from "../../assets/dragonLogo.png";
+import RedragonLogo from "../../assets/redragonLogo.png";
+import UbisoftLogo from "../../assets/ubisoftLogo.png";
 import Card from '../../Components/Cards/Card';
 
 function Home() {
@@ -22,8 +26,8 @@ function Home() {
         </div>
       </section>
 
-      <section className={styles.mediumSection}>
-        <div className={styles.mediumSectionText}>
+      <section className={styles.firstCardSection}>
+        <div className={styles.firstCardSectionText}>
           <h4>Faça parte da  comunidade [NOME]</h4>
           <span>Aqui você vai se sentir em casa ao participar da comunidade gamer</span>
         </div>
@@ -54,11 +58,35 @@ function Home() {
         </div>
       </section>
 
-      <section className={styles.bottomSection}>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+      <section className={styles.proplayerSection}>
+        <img src={Imagem02} />
+        <div className={styles.proplayerSectionText}>
+          <div className={styles.proplayerSectionTextH1}>
+            <h1>Se torne uma proplayer com a ProWoman</h1>
+          </div>
+          <div className={styles.proplayerSectionTextSpan}>
+            <span>Somos um projeto da comunidade [NOME] que incentiva equipes femininas de e-sports a competirem através de mentorias, promoção de competições, incentivo financeiro e mecânico.</span>
+          </div>
+          <div className={styles.proplayerSectionTextBtn}>
+            <Button className={styles.topSectionBtn} variant="primary">Quero Participar!</Button>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.patrocinadoresSection}>
+      <div className={styles.patrocinadoresSectionText}>
+          <h4>Empresas que apoiam e patrocinam a [NOME]</h4>
+          <span>Quer apoiar a comunidade?</span>
+        </div>
+        <div className={styles.patrocinadoresSectionImages}>
+          <img src={KabumLogo}/>
+          <img src={DragonLogo}/>
+          <img src={RedragonLogo}/>
+          <img src={UbisoftLogo}/>
+          <img src={UbisoftLogo}/>
+          <img src={UbisoftLogo}/>
+          <img src={UbisoftLogo}/>
+          <img src={UbisoftLogo}/>
         </div>
       </section>
     </div>
