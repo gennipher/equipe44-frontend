31aa485923507d6a5f7ba60c7302c105dddd088d
Ana Caroline <160973250+anaeggerdemier@users.noreply.github.com>
2024-05-26 15:46:02 -0300

    Update FormarEquipe.js

---
+++ src/Pages/FormarEquipe/FormarEquipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FormarEquipePage() {
+    return (
+        <div>
+            <h1>Você está prestes a formar equipe</h1>
+            <p>Para formar sua equipe, você pode usar nosso sistema de matching, entrar em uma equipe formada ou formar um time com outras jogadoras.</p>
+            {/* Adicione mais conteúdo conforme necessário */}
+        </div>
+    );
+}
+
+export default FormarEquipePage;
